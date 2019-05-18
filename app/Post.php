55b818a2fba25062f036778b209802b98c773c10
Prateek Dagur <deviscoding@gmail.com>
2019-05-18 12:56:57 +0530

    soft deletes table setup

--- app/Post.php
+++ app/Post.php
@@ -3,12 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
 
 class Post extends Model
 {
+	use softDeletes;
+
 	protected $fillable = [
 		'title', 'content', 'category_id', 'featured',
 	];
+
+	protected $dates = ['deleted_at'];
 
     public function category() {
     	return $this->belongsTo('App\Category');
