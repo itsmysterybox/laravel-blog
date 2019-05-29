59fca47f9910000a28ba8dd0ba5113df490e7749
Prateek Dagur <deviscoding@gmail.com>
2019-05-29 20:48:57 +0530

    dynamic content display on homepage

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Setting;
+use App\Category;
+use App\Post;
+use Illuminate\Http\Request;
+
+class FrontEndController extends Controller
+{
+    public function index() {
+    	return view('index')
+    			->with('categories', Category::take(5)->get())
+    			->with('first_post', Post::orderBy('created_at', 'desc')->first())
+    			->with('second_third_posts', Post::orderBy('created_at', 'desc')->skip(1)->take(2)->get())
+    			->with('latest_categories', Category::orderBy('updated_at', 'desc')->take(2)->get())
+    			->with('settings', Setting::first());
+    }
+}
