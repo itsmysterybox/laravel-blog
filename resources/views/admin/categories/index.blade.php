9b4ebef108413dff6b7f0978f91caf30f81e36c2
Prateek Dagur <deviscoding@gmail.com>
2019-06-04 21:57:14 +0530

    smaller button in categories panel

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -17,10 +17,10 @@
 								{{ $category->name }}
 							</td>
 							<td>
-								<a href="{{ route('category.edit', ['id' => $category->id ]) }}" class="btn btn-xs btn-info">Edit</a>
+								<a href="{{ route('category.edit', ['id' => $category->id ]) }}" class="btn btn-sm btn-info">Edit</a>
 							</td>
 							<td>
-								<a href="{{ route('category.delete', ['id' => $category->id ]) }}" class="btn btn-xs btn-danger">Delete</a>
+								<a href="{{ route('category.delete', ['id' => $category->id ]) }}" class="btn btn-sm btn-danger">Delete</a>
 							</td>
 						</tr>
 					@empty
